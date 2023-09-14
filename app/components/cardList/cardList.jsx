4db8578a0504e62c2dabc8fae99d40a558ba6208
Jatin singh <jatinsingh10001@gmail.com>
2023-09-14 14:24:09 +0530

    menu cardList

--- app/components/cardList/cardList.jsx
+++ app/components/cardList/cardList.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import styles from "./cardList.module.css"
+import Card from '../card/card'
 
 const CardList = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Recent Posts</h1>
       <div className={styles.posts}>
-         <div className={styles.imageContainer}></div>
-         <div className={styles.textContainer}></div>
+         <Card />
+         <Card />
+         <Card />
       </div>
     </div>
   )
